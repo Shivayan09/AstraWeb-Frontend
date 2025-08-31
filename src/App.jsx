cec6c5a32536e6c523720dae90b5b83f9b34ba1a
Shivayan Chakravarty <shivayanchak@gmail.com>
2025-08-31 17:47:06 +0530

    errors fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home.jsx'
-import BusinessEg from './pages/BusinessEg.jsx'
-import PortfolioEg from './pages/PortfolioEg.jsx'
 import ScrollToTop from './components/ScrollToTop.jsx'
 import Templates from './pages/Templates.jsx'
 
@@ -15,8 +13,6 @@ function App() {
     <ScrollToTop/>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/businessEg' element={<BusinessEg/>}/>
-        <Route path='/portfolioEg' element={<PortfolioEg/>}/>
         <Route path='/templates' element={<Templates/>}/>
       </Routes>
     </>
